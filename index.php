1757df6ec893ca7fb47d70e755fd86d680dfbe82
Alessandro <alessandro.boscato@gmail.com>
2020-10-02 16:45:08 +0200

    Fine esercizio

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *** -->
+
 <?php
-$nome_variabile = "Alessandro";
+$badword = $_GET["badword"];
+$paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$nuovo_paragrafo = str_replace($badword, "***", $paragrafo);
  ?>
 
-<h1>Ciao mi chiamo <?php echo $nome_variabile ?></h1>
+<h1><?php echo $nuovo_paragrafo?></h1>
