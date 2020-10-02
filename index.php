c073fe47c09236b627d0597a4721fa0d18000240
Alessandro <alessandro.boscato@gmail.com>
2020-10-02 16:50:35 +0200

    + strlen

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la ba
 $badword = $_GET["badword"];
 $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 $nuovo_paragrafo = str_replace($badword, "***", $paragrafo);
+$lunghezza_paragrafo = strlen($nuovo_paragrafo);
+
  ?>
 
 <h1><?php echo $nuovo_paragrafo?></h1>
+<h2>Lunghezza paragrafo: <?php echo $lunghezza_paragrafo ?></h2>
